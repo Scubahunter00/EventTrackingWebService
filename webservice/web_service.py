852ec8048789929098df712518ebe26fa4bebef4
Adam Jaggers <adam.jaggers@digitalreasoning.com>
2017-06-06 09:55:21 -0500

    adding 404 error for GET event

--- webservice/web_service.py
+++ webservice/web_service.py
@@ -1,10 +1,11 @@
-
-from flask import Flask, jsonify
+from datetime import date
+from flask import Flask, jsonify, abort
 from datastore.InMemoryEventStore import InMemoryEventStore
 
 app = Flask(__name__)
 
 data_store = InMemoryEventStore()
+current_id = 0
 
 event_types = ['purchase', 'deposit', 'education', 'life_event', 'hours_worked', 'education']
 
@@ -19,16 +20,41 @@ def get_event():
 
 @app.route('/event/api/events/<int:event_id>', methods=['GET'])
 def get_event_by_id(event_id):
+    event = data_store.get_event_by_id(event_id)
+    if not event:
+        abort(404)
     return jsonify({'events': data_store.get_event_by_id(event_id)})
 
-#@app.route('/event/api/event/', methods=['PUT'])
-#def put_event():
-#    return jsonify({'events': data_store.get_events()})
 
 @app.route('/event/api/types', methods=['GET'])
 def get_types():
     return jsonify({'event_types': event_types})
 
+
+def validate_event(event):
+
+    if event['id'] == None:
+        event['id'] = generate_id()
+    elif data_store.id_exists(event['id']):
+        raise ValueError('ID already exists')
+
+    if event['date'] == None:
+        event['date'] = date.today()
+
+    if event['type'] == None:
+        event['type'] = 'unknown'
+
+    if event['title'] == None:
+        raise ValueError('Event provided without title')
+
+
+
+
+def generate_id():
+    #TODO Use UUID Generation
+    global current_id
+    current_id +=1
+    return current_id
 
 if __name__ == '__main__':
     app.run(debug=True)
